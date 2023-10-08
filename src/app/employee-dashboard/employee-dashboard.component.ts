9d1686fa773e4048119c951f3ccf9509b7c90fed
revankale <rpkale.agpit@gmail.com>
2023-10-08 19:00:56 +0530

    add employee done, api create, install JSON server

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { EmployeeModel } from './employee';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -10,7 +12,9 @@ export class EmployeeDashboardComponent implements OnInit {
 
   formValue !: FormGroup;
 
-  constructor(private formbuilder: FormBuilder) { }
+  employeeObj: EmployeeModel = new EmployeeModel()
+
+  constructor(private formbuilder: FormBuilder, private api: ApiService) { }
 
   ngOnInit(): void {
     this.formValue = this.formbuilder.group({
@@ -20,6 +24,27 @@ export class EmployeeDashboardComponent implements OnInit {
       mobile: [''],
       salary: ['']
     })
+  }
+
+
+  postEmployeeDetails() {
+    this.employeeObj.firstName = this.formValue.value.firstName;
+    this.employeeObj.lastName = this.formValue.value.lastName;
+    this.employeeObj.email = this.formValue.value.email;
+    this.employeeObj.mobile = this.formValue.value.mobile;
+    this.employeeObj.salary = this.formValue.value.salary;
+
+    this.api.postEmployee(this.employeeObj)
+      .subscribe((res: any) => {
+        console.log(res);
+        alert("employee added successfully");
+        let ref = document.getElementById("cancel");
+        ref?.click();
+        this.formValue.reset();
+      },
+        err => {
+          alert("something wrong");
+        })
   }
 
 }
